63a289f1b9819d1b4853a5998d2928cdbee85f8e
Suylo <spoownz@yahoo.com>
2021-02-27 22:48:51 +0100

    ✔ JoinAndLeave Event fixed.

--- src/modules/events/joinAndLeave.rb
+++ src/modules/events/joinAndLeave.rb
@@ -2,12 +2,26 @@
 
 module JoinAndLeaveEvent
   extend Discordrb::EventContainer
+  extend Discordrb::Commands::CommandContainer
 
-  member_join do |event|
-    event.server.channel.send_message "#{event.user.name} has joined the server, welcome !"
+
+  member_join do |channel|
+    user_name = channel.user.username
+    user_avatar_url = channel.user.avatar_url('png')
+
+    channel.server.general_channel.send_embed do |embed|
+      embed.author = Discordrb::Webhooks::EmbedAuthor.new(name: "#{user_name} joined the server ! Welcome !", url: nil, icon_url: user_avatar_url)
+      embed.color = '#32CD32'
+    end
   end
 
-  member_leave do |event|
-    event.server.channel.send_message "#{event.user.name} has left the server, bye bye !"
+  member_leave do |channel|
+    user_name = channel.user.username
+    user_avatar_url = channel.user.avatar_url('png')
+
+    channel.server.general_channel.send_embed do |embed|
+      embed.author = Discordrb::Webhooks::EmbedAuthor.new(name: "#{user_name} left the server ! Bye !", url: nil, icon_url: user_avatar_url)
+      embed.color = '#FF0000'
+    end
   end
 end
