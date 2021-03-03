137632c8a56429e22536576ab058940b5040d057
Suylo <spoownz@yahoo.com>
2021-03-04 00:57:36 +0100

    ℹ URL updated & size improved.

--- src/modules/commands/getServerIconInfo.rb
+++ src/modules/commands/getServerIconInfo.rb
@@ -6,15 +6,15 @@ module GetServerIconInfo
 
   command :servericon do |event|
     server_name = event.server.name
-    server_icon_url = event.server.icon_url
+    server_id = event.server.id
     server_icon_id = event.server.icon_id
     user_mention = event.user.mention
     user_distinct = event.user.distinct
 
     # rubocop:disable Layout/LineLength
-    event.respond "Hi #{user_mention} ! Here is the URL and ID of the server icon : *#{server_name}* \n\n**URL:** https://cdn.discordapp.com/icons/#{event.server.id}/#{server_icon_id}.png?size=512\n**ID:** #{server_icon_id}"
+    event.respond "Hi #{user_mention} ! Here is the URL and ID of the server icon : *#{server_name}* \n\n**URL:** https://cdn.discordapp.com/icons/#{server_id}/#{server_icon_id}.png?size=512\n**ID:** #{server_icon_id}"
     # rubocop:enable Layout/LineLength
-    puts "GetServerIcon_URL executed by #{user_distinct} on the server : #{server_name}"
+    puts "GetServerIcon executed by #{user_distinct} on the server : #{server_name}"
   end
 end
 
