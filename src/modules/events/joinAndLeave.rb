92943b49c85e4b4aac764aacf9a9d3ed525ea4b4
Suylo <spoownz@yahoo.com>
2021-03-04 01:19:18 +0100

    ✔ Join and Leave context improved.

--- src/modules/events/joinAndLeave.rb
+++ src/modules/events/joinAndLeave.rb
@@ -3,25 +3,33 @@
 module JoinAndLeaveEvent
   extend Discordrb::EventContainer
   extend Discordrb::Commands::CommandContainer
+  DWB = Discordrb::Webhooks
 
 
   member_join do |channel|
-    user_name = channel.user.username
-    user_avatar_url = channel.user.avatar_url('png')
-
     channel.server.general_channel.send_embed do |embed|
-      embed.author = Discordrb::Webhooks::EmbedAuthor.new(name: "#{user_name} joined the server ! Welcome !", url: nil, icon_url: user_avatar_url)
-      embed.color = '#32CD32'
+      embed.title = "#{channel.user.username} joined the server! Welcome!"
+      embed.image = DWB::EmbedImage.new(url: "https://cdn.discordapp.com/avatars/#{channel.user.id}/#{channel.user.avatar_id}.png?size=256")
+      embed.timestamp = Time.now
+      embed.colour = '#5beb78'
+      embed.footer = DWB::EmbedFooter.new(text: "##{channel.user.tag}", icon_url: nil)
     end
+
+    puts "#{user_tag} left the server : #{channel.server.name} !"
+
   end
 
-  member_leave do |channel|
-    user_name = channel.user.username
-    user_avatar_url = channel.user.avatar_url('png')
 
+  member_leave do |channel|
     channel.server.general_channel.send_embed do |embed|
-      embed.author = Discordrb::Webhooks::EmbedAuthor.new(name: "#{user_name} left the server ! Bye !", url: nil, icon_url: user_avatar_url)
-      embed.color = '#FF0000'
+      embed.title = "#{channel.user.username} left the server! Bye bye!"
+      embed.image = DWB::EmbedImage.new(url: "https://cdn.discordapp.com/avatars/#{channel.user.id}/#{channel.user.avatar_id}.png?size=256")
+      embed.timestamp = Time.now
+      embed.colour = '#d64d4d'
+      embed.footer = DWB::EmbedFooter.new(text: "##{channel.user.tag}", icon_url: nil)
     end
+
+    puts "#{channel.user.distinct} joined the server : #{channel.server.name} !"
+
   end
 end
